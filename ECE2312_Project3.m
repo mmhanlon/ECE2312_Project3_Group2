1706cde0d0c2224930966914ad9d98ccdf89a887
Jacob Nguyen <123837162+jvnguyen102@users.noreply.github.com>
2023-03-01 13:17:20 -0500

    Update ECE2312_Project3.m

--- ECE2312_Project3.m
+++ ECE2312_Project3.m
@@ -37,3 +37,13 @@ xH = downsample(high_pass,2);
 
 plotSpec_subplot(low_pass,high_pass, 'Lowpass', 'Highpass', fs);
 plotSpec_subplot(xL,xH, 'Downsampled Lowpass', ' Downsampled Highpass', fs);
+
+% Second Frequency Decomposition
+low_pass2 = filter(lpf, 1, low_pass);
+xL2 = downsample(low_pass2,2);
+
+high_pass2 = filter(hpf, 1, high_pass);
+xH2 = downsample(high_pass2,2);
+
+plotSpec_subplot(low_pass2,high_pass2, 'Lowpass 2', 'Highpass 2', fs);
+plotSpec_subplot(xL2,xH2, 'Downsampled Lowpass 2', ' Downsampled Highpass 2', fs);
